4308f44bcf4b707610d57ed8ab2d4e30f79389b0
Nikolay Kluchnikov <yndx-anakinskw@yandex.ru>
2024-09-24 00:01:50 +0300

    "add class_into_tests"

--- tests/test_get_list_order.py
+++ tests/test_get_list_order.py
@@ -3,8 +3,10 @@ import requests
 from src.data import Urls
 
 @allure.suite('Проверяем возврат списка заказов')
-@allure.title('Проверяем код и тело ответа при запросе списка заказов')
-def test_get_list_order():
-    response = requests.get(f"{Urls.BASE_URL}{Urls.GET_ORDER}")
-    assert 200 == response.status_code and 'orders' in response.json()
+class TestGetlistOrder:
+
+    @allure.title('Проверяем код и тело ответа при запросе списка заказов')
+    def test_get_list_order(self):
+        response = requests.get(f"{Urls.BASE_URL}{Urls.GET_ORDER}")
+        assert 200 == response.status_code and 'orders' in response.json()
 
--- tests/test_order.py
+++ tests/test_order.py
@@ -4,20 +4,22 @@ import pytest
 from src.data import OderInfo, Urls
 
 @allure.suite('Проверяем заказ самоката')
-@allure.title('Проверяем код и тело ответа при заказе самоката разных цветов - Черный, Серый, Черный и Серый, Цвет не указан')
-@pytest.mark.parametrize('colors', ['BLACK', 'GREY', 'BLACK, GREY', ''])
-def test_order_colors_scooter(colors):
-    order_data = {
-            "firstName": OderInfo.firstName,
-            "lastName": OderInfo.lastName,
-            "address": OderInfo.address,
-            "metroStation": OderInfo.metroStation,
-            "phone": OderInfo.phone,
-            "rentTime": OderInfo.rentTime,
-            "deliveryDate": OderInfo.deliveryDate,
-            "comment": OderInfo.comment,
-            "color": [colors]
-    }
-    response = requests.post(f"{Urls.BASE_URL}{Urls.ORDER}", json=order_data)
-    assert 201 == response.status_code and 'track' in response.json()
+class TestOrder:
+
+    @allure.title('Проверяем код и тело ответа при заказе самоката разных цветов - Черный, Серый, Черный и Серый, Цвет не указан')
+    @pytest.mark.parametrize('colors', ['BLACK', 'GREY', 'BLACK, GREY', ''])
+    def test_order_colors_scooter(self, colors):
+        order_data = {
+                "firstName": OderInfo.firstName,
+                "lastName": OderInfo.lastName,
+                "address": OderInfo.address,
+                "metroStation": OderInfo.metroStation,
+                "phone": OderInfo.phone,
+                "rentTime": OderInfo.rentTime,
+                "deliveryDate": OderInfo.deliveryDate,
+                "comment": OderInfo.comment,
+                "color": [colors]
+        }
+        response = requests.post(f"{Urls.BASE_URL}{Urls.ORDER}", json=order_data)
+        assert 201 == response.status_code and 'track' in response.json()
 
